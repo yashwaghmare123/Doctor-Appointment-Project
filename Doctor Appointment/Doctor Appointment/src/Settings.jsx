728a5f2a77eacb8bff1932f56c240d8c0e111178
Kashaf1175 <srinidhiandure@gmail.com>
2024-09-16 23:32:20 +0530

    modified

--- Doctor Appointment/Doctor Appointment/src/Settings.jsx
+++ Doctor Appointment/Doctor Appointment/src/Settings.jsx
@@ -1,6 +1,5 @@
 // src/Header.jsx
 import React from 'react';
-import { Link } from "react-router-dom"
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Import Bootstrap JavaScript for dropdowns
 
@@ -10,6 +9,7 @@ function Header() {
     <div>
       <h1>Settings</h1>
       <h2>htrr</h2>
+      <h3></h3>
     </div>
        
   
